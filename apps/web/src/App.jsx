cd971ebd1bfd0178e556f85b7561f5bec9ddac71
acilxrembo-png <acilxrembo@gmail.com>
2026-09-24 23:25:36 +0700

    first commit

---
+++ apps/web/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-3xl font-bold text-blue-500">
+      bdakbdwb
+    </div>
+  )
+}
+
+export default App
